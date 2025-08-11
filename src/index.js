26374dcafb7c055652ca1229d5345d560cedf4da
Blastonn <felipe.paulo07@hotmail.com>
2025-08-11 16:19:41 -0300

    Feat- Adicionar informacoes de contato exemplo
    Adicionado exemplo de informacoes para contato.

--- src/index.js
+++ src/index.js
@@ -19,3 +19,4 @@ botaoSobre.addEventListener("click",criarSobre);
 
 
 
+
--- src/paginaSobre.js
+++ src/paginaSobre.js
@@ -2,9 +2,13 @@ export function criarSobre(){
     const divContent = document.querySelector("div#content");
     divContent.innerHTML = "";
     const divContato = document.createElement("div");
+    const paraContato = document.createElement("p");
     divContato.classList.add("contato");
+    paraContato.textContent = "(XX)XXXXXXX";
+
     const titulo = document.createElement("h1"); 
-    titulo.textContent = "Sobre";
+    titulo.textContent = "Contato";
     divContent.append(titulo,divContato);
+    divContato.appendChild(paraContato);
 
 }
